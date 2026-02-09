52d86a0dad4151e6986b9630ed4fe613f966a17d
ezi-code <emailyendau2000@hotmail.com>
2026-02-09 11:11:55 +0000

    configure auto settig file swith and create production database

--- myOrderFellow/settings/base.py
+++ myOrderFellow/settings/base.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 AUTH_USER_MODEL = "users.User"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
--- myOrderFellow/settings/production.py
+++ myOrderFellow/settings/production.py
@@ -16,7 +16,7 @@ ALLOWED_HOSTS = ["myorderfellow.onrender.com"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # noqa F405
+        "NAME": BASE_DIR / "db.sqlite3.production",  # noqa F405
     }
 }
 
